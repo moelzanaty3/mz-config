132e649b8fe1d910e69c774f6375ce84172b5848
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-06-05 21:48:30 +0200

    fix root app path

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,11 @@
 import os from 'os'
+import path from 'path'
 
 const { username } = os.userInfo()
 
+const appRoot = path.dirname(require.main?.filename as string)
+
 export default {
-  username
+  username,
+  appRoot
 }
--- src/modules/typescript.module.ts
+++ src/modules/typescript.module.ts
@@ -22,7 +22,7 @@ const createTypeScriptFile = async (framework: string) => {
     )
   )
 
-  const configFolderPath = path.join(process.cwd(), 'src', 'config')
+  const configFolderPath = path.join(constants.appRoot, 'src', 'config')
   // get path's for all frameworks typescript config
   const frameworksConfigFiles: Frameworks =
     (await utils.getConfigFiles(configFolderPath)) || ({} as Frameworks)
@@ -74,3 +74,7 @@ const createTypeScriptFile = async (framework: string) => {
 }
 
 export default createTypeScriptFile
+function appRoot(appRoot: any, arg1: string, arg2: string) {
+  throw new Error('Function not implemented.')
+}
+
